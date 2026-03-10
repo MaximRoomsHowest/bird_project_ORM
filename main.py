a27e6bb560a1e388515ebeb5d85cc9992167db0f
MaximRoomsHowest <maxim.rooms@student.howest.be>
2026-03-10 18:37:34 +0100

    structure: basic ORM for species added

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from database import start_db  # your database setup
+from routers.species import router as species_router
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    start_db()
+    yield
 
-app = FastAPI()
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "API is running"}
+
+app.include_router(species_router)
